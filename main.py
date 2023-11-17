5ebe0962a434c5a6c1917833f207210e281dc0fc
obahortifrutiramon <134549091+obahortifrutiramon@users.noreply.github.com>
2023-11-17 18:03:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -30,14 +30,14 @@ personalPassword = st.secrets["personal_Password"]
 
 
 
-df = df_skus.merge(df_estoque, how='left', on=['SEQPRODUTO'])
-df['ESTOQUE'] = df['ESTQLOJA'] - df['QTDRESERVADAVDA']
-df['EXCEDENTE'] = df['ESTOQUE'] - df['QUANTIDADE']
-df = df.sort_values(['PRODUTO','NROEMPRESA','EXCEDENTE'])
-df['EXCMIN'] = df.groupby(['PRODUTO','NROEMPRESA'])['EXCEDENTE'].transform('first')
+#df = df_skus.merge(df_estoque, how='left', on=['SEQPRODUTO'])
+#df['ESTOQUE'] = df['ESTQLOJA'] - df['QTDRESERVADAVDA']
+#df['EXCEDENTE'] = df['ESTOQUE'] - df['QUANTIDADE']
+#df = df.sort_values(['PRODUTO','NROEMPRESA','EXCEDENTE'])
+#df['EXCMIN'] = df.groupby(['PRODUTO','NROEMPRESA'])['EXCEDENTE'].transform('first')
 
-df_kits = df[['SKU VTEX','PRODUTO','NROEMPRESA','EXCMIN']].drop_duplicates()
-df_prod = df[['SEQPRODUTO','PRODUTO','NROEMPRESA','EXCEDENTE']].drop_duplicates()
+#df_kits = df[['SKU VTEX','PRODUTO','NROEMPRESA','EXCMIN']].drop_duplicates()
+#df_prod = df[['SEQPRODUTO','PRODUTO','NROEMPRESA','EXCEDENTE']].drop_duplicates()
 
 # def plot_grafico(dataframe):
 #     fig, ax = plt.subplots()
@@ -51,8 +51,8 @@ if __name__ == "__main__":
     st.set_page_config(page_title="Acompanhamento de Estoque")
     st.title("Acompanhamento de Estoque\n")
     
-    df_kitsalerta = df_kits[df_kits['EXCMIN'] < 5]
-    st.bar_chart(data=df_kitsalerta, x='PRODUTO', y=['EXCMIN'], color=None, width=0, height=0)
+    #df_kitsalerta = df_kits[df_kits['EXCMIN'] < 5]
+    #st.bar_chart(data=df_kitsalerta, x='PRODUTO', y=['EXCMIN'], color=None, width=0, height=0)
     
 #     figura = plot_grafico(df.head(5))
 #     st.pyplot(figura)
