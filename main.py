05e9077ea9afa1f9e8f067c80cde0c6e9e28a12b
obahortifrutiramon <134549091+obahortifrutiramon@users.noreply.github.com>
2023-11-21 18:26:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,31 +17,24 @@ cnopts.hostkeys = None
 sftp = pysftp.Connection(host=myHostname, port=port, username=myUsername, password=myPassword, cnopts=cnopts)
 
 
-# Kits
-with sftp.open('/dados/BF/kits.csv', 'r') as remote_file:
-    contents_bytes = remote_file.read()
-
-contents_str = contents_bytes.decode('utf-8')  # Converter bytes para string
-dfk = pd.read_csv(StringIO(contents_str), sep=';')
-dfk = dfk[(dfk['SKU VTEX'] != 'SKU') & (dfk['NROEMPRESA'] == 1)]
-
-
-
 # Produtos
 with sftp.open('/dados/BF/produtos.csv', 'r') as remote_file:
     contents_bytes = remote_file.read()
 
 contents_str = contents_bytes.decode('utf-8')  # Converter bytes para string
 dfp = pd.read_csv(StringIO(contents_str), sep=';')
+dfp['EXCEDENTE'] = dfp['EXCEDENTE'].str.replace(',', '.').astype(float)
+dfp['ESTOQUE'] = dfp['ESTOQUE'].str.replace(',', '.').astype(float)
+dfp = dfp.sort_values('EXCEDENTE')
+dfp['SEQPRODUTO'] = dfp['SEQPRODUTO'].astype(str)
 
-
-
+df = dfp[['EMPRESA','SEQPRODUTO','DESCCOMPLETA','EXCEDENTE']].head(20)
 
 
 # STREAMLIT
 # Gráfico interativo usando Plotly Express
-fig = px.bar(dfk, x='SKU VTEX', y='EXCEDENTE', title='Excedente por KIT',
-             labels={'EXCEDENTE': 'Quantidade excedente', 'SKU VTEX': 'ID do kits na vtex'})
+fig = px.bar(df, x='SEQPRODUTO', y='EXCEDENTE', title='Excedente por SKU',
+             labels={'EXCEDENTE': 'Quantidade excedente', 'SEQPRODUTO': 'SKU'})
 
 # Exibir o gráfico no Streamlit
 st.plotly_chart(fig)
